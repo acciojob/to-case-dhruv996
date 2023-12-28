b358622d2eec55a8974a916c55af4a2986056796
Dhruv Varshney <144780592+dhruv996@users.noreply.github.com>
2023-12-28 14:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function toCase(text) {
-  // write your code here
+document.addEventListener('DOMContentLoaded', function () {
+  // Example usage
+  const result1 = toCase('Mthatha');
+  document.getElementById('result').innerText = `Result for 'Mthatha': '${result1}'`;
+
+  const result2 = toCase('HelloWorld');
+  document.getElementById('result').innerText += `\nResult for 'HelloWorld': '${result2}'`;
+
+  const result3 = toCase('OpenAI');
+  document.getElementById('result').innerText += `\nResult for 'OpenAI': '${result3}'`;
+});
+
+function toCase(str) {
+  if (!str) {
+    return '-';
+  }
+
+  const lowerCaseStr = str.toLowerCase();
+  const upperCaseStr = str.toUpperCase();
+
+  return `${lowerCaseStr}-${upperCaseStr}`;
 }
 
-// DO not change the code below
-
-const text = prompt("Enter text:");
-alert(toCase(text));
